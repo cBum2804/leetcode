8823e9200e367d18851635dceaa1fbac7d922ca5
Dhruv Bhandary <119063618+cBum2804@users.noreply.github.com>
2025-07-19 18:21:47 +0530

    Time: 1857 ms (18.60%) | Memory: 92.8 MB (13.18%) - LeetSync

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestSubstring(self, s, k):
+        if len(s) < k:
+            return 0
+        
+        char_freq = {}
+        for char in s:
+            if char not in char_freq:
+                char_freq[char] = 1
+            else:
+                char_freq[char] +=1
+        
+        for i, char in enumerate(s):
+            if char_freq[char]<k:
+                left = self.longestSubstring(s[:i], k)
+                right =self.longestSubstring(s[i+1:], k)
+                return max(left, right)
+        
+        return len(s)
+        
