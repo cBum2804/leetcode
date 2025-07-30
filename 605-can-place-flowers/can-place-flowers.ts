8b0f7cfeaaea35afdf2c3b62c43ee436d0b3a59d
Dhruv Bhandary <119063618+cBum2804@users.noreply.github.com>
2025-07-30 22:48:32 +0530

    Time: 1 ms (85.47%) | Memory: 58.1 MB (47.83%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.ts
@@ -0,0 +1,15 @@
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+    if(n===0){
+        return true;
+    }
+    for(let i=0; i<=flowerbed.length; i++){
+        if(flowerbed[i]===0 && (i===0 || flowerbed[i-1]===0) && (i===flowerbed.length-1 || flowerbed[i+1]===0)){
+            n--;
+            flowerbed[i]=1;
+        }
+        if(n===0){
+            return true;
+        }
+    }
+    return false
+};
