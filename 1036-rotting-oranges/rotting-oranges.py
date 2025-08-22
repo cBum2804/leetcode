d16eb6fea73a5429c9ceec551dc399e23f132f02
Dhruv Bhandary <119063618+cBum2804@users.noreply.github.com>
2025-08-22 18:42:36 +0530

    Time: 3 ms (81.41%) | Memory: 17.6 MB (99.25%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,32 +1,36 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        row,col = len(grid), len(grid[0])
-        EMPTY, FRESH, ROTTEN = 0,1,2
-        fresh_count=0
         q = deque()
+        fresh_count=0
+        EMPTY, FRESH, ROTTEN = 0,1,2
+        row,col = len(grid), len(grid[0])
+        seen = set()
 
         for i in range(row):
             for j in range(col):
-                if grid[i][j] == FRESH:
-                    fresh_count+=1
-                elif grid[i][j] == ROTTEN:
+                if grid[i][j]==ROTTEN:
                     q.append((i,j))
+        
+                elif grid[i][j] == FRESH:
+                    fresh_count +=1
 
-        if fresh_count ==0:
+        if fresh_count==0:
             return 0
-        minutes = -1
+        num_minutes=-1
         while q:
-            len_q = len(q)
-            minutes+=1
-            for _ in range(len_q):
-                x, y = q.popleft()
-                for r,c in [(x,y+1),(x,y-1),(x+1,y),(x-1,y)]:
-                    if r>=0 and r<row and c>=0 and c<col and grid[r][c]== FRESH:
-                        grid[r][c]= ROTTEN
+            num_minutes+=1
+            q_size = len(q)
+
+            for _ in range(q_size):
+                i, j = q.popleft()
+                for r,c in [(i,j+1),(i,j-1),(i+1, j),(i-1,j)]:
+                    if r>=0 and r< row and c>=0 and c< col and grid[r][c]== FRESH and (r,c)not in seen:
                         q.append((r,c))
+                        seen.add((r,c))
                         fresh_count-=1
-        if fresh_count == 0:
-            return minutes
+                        grid[r][c]= ROTTEN
+        if fresh_count==0:
+            return num_minutes
         else:
             return -1
-        
+
